2520393507730ee9006b90cb82187451e32457d2
jovinroshanj <jovin.roshan.dev@gmail.com>
2025-06-03 02:43:01 +0530

    add search functionality

--- index.js
+++ index.js
@@ -32,9 +32,23 @@ app.use(cors({
 }));
 
 
+
 // Connect MongoDB
 connectDB()
 
+app.delete("/delete-product/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedProduct = await StockManagement.findByIdAndDelete(id);
+        if (!deletedProduct) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json({ message: "Product deleted successfully" });
+    } catch (error) {
+        console.error("Error deleting product:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
 
 app.use("/cart",cartRoutes)
 
